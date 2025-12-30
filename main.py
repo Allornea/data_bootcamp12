9e96071a9df9cea4c9b93b148f5ce23eb9c27f57
Allornea <ookza5949@gmail.com>
2025-12-31 01:50:56 +0700

    github is ez

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 #print hello world
-print("Hello World")
+print("Now I know it already")
 
-print("I hate gay")
-
-print("I love you")
-
-print("I love you too")
-
-def add_two(num):
-    return num + 2
-
-print(add_two(2),add_two(5))
